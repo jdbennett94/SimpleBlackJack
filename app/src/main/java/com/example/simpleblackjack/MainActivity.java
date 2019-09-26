6ada9be8c6ecbd642c223dfa1486f81b910cc174
jdbennett94 <jdbennett@loyola.edu>
2019-09-25 20:24:19 -0400

    Monday/old initial parameter deployment

--- app/src/main/java/com/example/simpleblackjack/MainActivity.java
+++ app/src/main/java/com/example/simpleblackjack/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.simpleblackjack;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    Button button = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Start Game Button
+        button = (Button)findViewById(R.id.button1);
+        button.setOnClickListener( new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                //Deck initialized and shuffled
+
+                //Dealer given two cards
+
+                //Player given two cards
+
+                //Check for 21 on both (method)
+
+                //Option for player to hit/stay
+
+                //Check over 21
+
+                //Option to hit stay
+
+                //Dealer draw
+
+
+            }
+        });
+
+
     }
 }
