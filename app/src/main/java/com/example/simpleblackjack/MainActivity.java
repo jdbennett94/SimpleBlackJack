ed542f4fc0fca4a2d15a5eb40d0bac87da6db080
jdbennett94 <jdbennett@loyola.edu>
2019-09-29 18:36:09 -0400

    Multiple prototype java methods from earlier this week 9/26: -Check if blackjack -Convert string value of cards -Etc.

--- app/src/main/java/com/example/simpleblackjack/MainActivity.java
+++ app/src/main/java/com/example/simpleblackjack/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
                 //Deck initialized and shuffled
 
+
                 //Dealer given two cards
 
                 //Player given two cards
@@ -44,4 +45,101 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    /**
+     *
+     * @return
+     */
+     public String[] deck() {
+
+         String[] deck = new String[];
+
+         return deck;
+
+     }
+
+
+    /**
+     * Changes the string value of the card to a int value for totalling
+     *
+     * @param card
+     * @return
+     */
+    public int cardValue(String card) {
+
+        int cardVal;
+
+        // Splits line (card name ex. ace of diamionds) into individual words within an array (ie.
+        // splitStr[0] = "ace", splitStr[1]="of", splitStr[2]="diamonds"
+        String[] splitStr = card.split("\\s+");
+
+        // splitStr[0] holds first word of each card name (ex. ACE of hearts, TEN of diamonds, etc)
+        // We convert that index to int for value
+        if(splitStr[0].equals("Ace") || splitStr[0].equals("King") || splitStr[0].equals("Queen") || splitStr[0].equals("Jack")) {
+
+            cardVal = 10;
+
+        }
+        else {
+
+            int changeValue = Integer.parseInt(splitStr[0]);
+            cardVal = changeValue;
+
+        }
+
+        return cardVal;
+
+     }
+
+
+    /**
+     *
+     * Determines if player or dealer hits blackjack (21)
+     *
+     * @return int
+     */
+    public boolean blackjackCheck(int total){
+
+        boolean gameover = false;
+
+        if (total == 21)
+        {
+            gameover = true;
+        }
+
+        return gameover;
+
+     }
+
+
+    /**
+     *  Determines if player wins, loses or ties
+     *
+     * @param player
+     * @param dealer
+     * @return String;
+     */
+    public String winner (int player, int dealer) {
+
+        String win = "";
+
+        if (player > dealer)
+        {
+            win = "Congrats you've won!";
+        }
+        else if(player == dealer)
+        {
+            win = "It's a tie!";
+        }
+        else
+            win = "dealer wins";
+
+        return win;
+
+     }
+
+
+
+
+
 }
