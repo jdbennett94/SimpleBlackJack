d132af20fc19adbd227b08d0c222ce15ace583ce
Chiara <cmmaalouf@loyola.edu>
2019-09-30 17:22:52 -0400

    made onstart method and connected it to button

---
+++ app/src/main/java/com/example/simpleblackjack/GameActivity.java
@@ -0,0 +1,4 @@
+package com.example.simpleblackjack;
+
+public class GameActivity {
+}
--- app/src/main/java/com/example/simpleblackjack/MainActivity.java
+++ app/src/main/java/com/example/simpleblackjack/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.simpleblackjack;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,13 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void StartGame(View view) {
+        Intent myIntent = new Intent( this,
+                GameActivity.class );
+        this.startActivity( myIntent );
 
 
+    }
         //Start Game Button
-        button = (Button)findViewById(R.id.button1);
-        button.setOnClickListener( new View.OnClickListener() {
-
-            public void onClick(View v) {
 
 
 
